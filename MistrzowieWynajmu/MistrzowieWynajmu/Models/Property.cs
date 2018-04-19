8f1e84df69116db0f68a873903e97eb48bdccae3
Maciej <mzubrzycki@op.pl>
2018-04-19 21:59:30 +0200

    adding models and class address, owner, proprerty for ther first time

---
+++ MistrzowieWynajmu/MistrzowieWynajmu/Models/Property.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MistrzowieWynajmu.Models
+{
+    public class Property
+    {
+		public int Id { get; set; }
+		public PropertyType Type { get; set; }
+		public string Descryption { get; set; }
+		public int Rooms { get; set; }
+		public bool Washer { get; set; }
+		public bool Referigerator { get; set; }
+		public bool Iron { get; set; }
+
+		public virtual int AddressId { get; set; }
+		public virtual Address Address { get; set; }
+
+		public virtual int OwnerId { get; set; }
+		public virtual Owner Owner { get; set; }
+	}
+	public enum PropertyType
+	{
+		House = 0,
+		Flat = 1
+	};
+}
